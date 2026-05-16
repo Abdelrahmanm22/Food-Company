d8ef30591fae34903580eef6df0c4eb1dfa9c0fc
Abdelrahmanm22 <abdra1396@gmail.com>
2026-05-16 17:44:49 +0300

    build generic repo

---
+++ Food.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Food.Domain.Models;
+
+namespace Food.Domain.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseModel
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+    }
+}
---
+++ Food.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Food.Domain.Models;
+using Food.Domain.Repositories;
+using Food.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Food.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseModel
+    {
+        private readonly FoodContext dbContext;
+
+        public GenericRepository(FoodContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FirstAsync(r => r.Id == id);
+        }
+    }
+}
