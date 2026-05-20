84daf9fdc489ecbce514d7c7177dbe8d41428564
Abdelrahmanm22 <abdra1396@gmail.com>
2026-05-20 13:25:51 +0300

    add login functionality with user authentication

--- Food.APIs/Controllers/AccountsController.cs
+++ Food.APIs/Controllers/AccountsController.cs
@@ -10,10 +10,12 @@ namespace Food.APIs.Controllers
     public class AccountsController : APIBaseController
     {
         private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
 
-        public AccountsController(UserManager<AppUser> userManager)
+        public AccountsController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
         [HttpPost("Register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto model)
@@ -32,9 +34,25 @@ namespace Food.APIs.Controllers
             {
                 UserName = User.UserName,
                 Email = User.Email,
-                Token = "This is a token for " + User.UserName
+                Token = $"This is a token for {User.UserName}"
+            };
+            return ReturnedUser;
+        } 
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto model)
+        {
+            var User = await userManager.FindByEmailAsync(model.Email);
+            if (User is null) return Unauthorized(new ApiErrorResponse(401, "Invalid Email or Password"));
+            var Result = await signInManager.CheckPasswordSignInAsync(User, model.Password, false);
+            if(!Result.Succeeded) return Unauthorized(new ApiErrorResponse(401, "Invalid Email or Password"));
+            var ReturnedUser = new UserDto()
+            {
+                UserName = User.UserName,
+                Email = User.Email,
+                Token = $"This is a token for {User.UserName}"
             };
             return ReturnedUser;
         }
+
     }
 }
---
+++ Food.APIs/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Food.APIs.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
